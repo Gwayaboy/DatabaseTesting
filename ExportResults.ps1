6035d21bd100811c11f001bdfa180a7cf8ea3e31
Franck Théolade <ftheolade@gmail.com>
2020-11-28 15:58:45 +0000

    updated script

--- ExportResults.ps1
+++ ExportResults.ps1
@@ -9,6 +9,24 @@
 
 Write-Output "Exporting to tSQLt run to $testResultsFileName"    
 
-(Invoke-SqlCmd -ConnectionString $connectionString -QueryTimeout $queryTimeout -Query "EXEC [tSQLt].[XMLResultFormatter]")[0] | Out-File -FilePath "$testResultsFileName" -NoNewLine
+$destinationDirectory = Split-Path -Path $testResultsFileName
+
+if (-not (Test-Path -LiteralPath $destinationDirectory)) {
+    
+    try {
+        New-Item -Path $destinationDirectory -ItemType Directory -ErrorAction Stop | Out-Null #-Force
+    }
+    catch {
+        Write-Error -Message "Unable to create directory '$destinationDirectory'. Error was: $_" -ErrorAction Stop
+    }
+    Write-Output "Successfully created directory '$destinationDirectory'."
+
+}
+else {
+    Write-Output "Directory already existed"
+}
+
+
+(Invoke-SqlCmd -ConnectionString $connectionString -QueryTimeout $queryTimeout -Query "EXEC [tSQLt].[XMLResultFormatter]")[0] | Out-File -FilePath $testResultsFileName -NoNewLine
 
 Write-Output "Exported test run to $testResultsFileName"    
