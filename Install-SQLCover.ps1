aaf6a491b5106753fc1d3ee45c1252dab0ea7195
Franck Théolade <ftheolade@gmail.com>
2020-11-28 17:47:13 +0000

    update instal cover script to create folder if does not exists

--- Install-SQLCover.ps1
+++ Install-SQLCover.ps1
@@ -1,10 +1,15 @@
 ﻿param(
     [String] $releaseUrl = "https://github.com/GoEddie/SQLCover/archive/0.5.0.zip",
-    [string]$destinationFolder = "C:\temp\"
+    [string]$destinationFolder = "C:\temp\blah"
 )
 
-$zipFile = $destinationFolder + $(Split-Path -Path $releaseUrl -Leaf) 
- 
+$zipFile = $destinationFolder + $(Split-Path -Path $releaseUrl -Leaf)
+$executingScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent 
+
+. $executingScriptDirectory\CreateDirectoryIfDoesNotExist.ps1 -destinationDirectory $destinationFolder
+
+Write-Output "Downloading $releaseUrl to $zipFile"    
+
 Invoke-WebRequest -Uri $releaseUrl -OutFile $zipFile
 
 Expand-Archive -LiteralPath $zipFile -DestinationPath $destinationFolder -Force
