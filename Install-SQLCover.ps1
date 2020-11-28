6b39fc325a5bc61db394edd6ddcd21cdd21b408c
Franck Théolade <ftheolade@gmail.com>
2020-11-28 15:01:59 +0000

    Added custom script to get results

---
+++ Install-SQLCover.ps1
@@ -0,0 +1,13 @@
+﻿param(
+    [string]$dependenciesDirectory = "C:\Dev"
+)
+
+$Url = 'https://github.com/GoEddie/SQLCover/archive/0.5.0.zip' 
+$ZipFile = $dependenciesDirectory + $(Split-Path -Path $Url -Leaf) 
+$Destination= $dependenciesDirectory + '\SQLCover\'
+ 
+Invoke-WebRequest -Uri $Url -OutFile $ZipFile 
+ 
+$ExtractShell = New-Object -ComObject Shell.Application 
+$Files = $ExtractShell.Namespace($ZipFile).Items() 
+$ExtractShell.NameSpace($Destination).CopyHere($Files) 
