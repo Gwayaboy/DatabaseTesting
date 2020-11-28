263bcc63298be495e2ea982fdae932e8f2b83394
Franck Théolade <ftheolade@gmail.com>
2020-11-28 15:04:56 +0000

    Added custom script to get results

---
+++ ExportResults.ps1
@@ -0,0 +1,12 @@
+﻿param(
+    # Database info parameters    
+    [string]$connectionString,
+    
+    # Test Result parameters
+    [string]$testResultsFileName,
+    [string]$queryTimeout
+)
+
+mkdir tsqlt-out
+cd tSQLt-out
+(Invoke-SqlCmd -ConnectionString $connectionString -QueryTimeout $queryTimeout -Query "EXEC [tSQLt].[XMLResultFormatter]")[0] | Out-File -FilePath $testResultsFileName -NoNewLine
